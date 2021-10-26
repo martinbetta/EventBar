1a67b4e038b9f290ba80faf2e4ec88b882911a3f
Martin Bettaglio <martin.bettaglio@gmail.com>
2021-10-26 17:22:24 +0200

    delete

---
+++ assets/scripts/productos.js
@@ -0,0 +1,71 @@
+//////////Productos///////////////////
+// class Bebidas {
+//   constructor(nombre, image, litros, precio){
+//  this.nombre = nombre;
+//  this.image = image;
+//  this.litros = Number(litros);
+//  this.precio = precio
+// }
+// }
+
+const Productos = [
+	{
+	id: 1,
+	nombre: 'Ballantines',
+	precio: 1,
+	imagen: 'ballantines-finest-whisky_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+	{
+	id: 1,
+	nombre: 'Coca-Cola',
+	precio: 1,
+	imagen: 'bombay-ginebra-70-cl_24h.jpg',
+	litros: 1,
+	},
+]
+
+
+export {  };
